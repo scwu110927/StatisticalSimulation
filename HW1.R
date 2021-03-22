2cfe79e00f83ce24b49c4db7b1932a18bd5573dd
scwu110927 <scwu110927@gmail.com>
2021-03-22 11:43:02 +0800

    Update HW1.R

--- HW1.R
+++ HW1.R
@@ -1,16 +1,16 @@
 #1(a)
 rep(seq(0,4),each=5)
 
-#(b)
+#1(b)
 seq(1:5) + rep(0:4,each=5)
 
-#(C) 123234345456
+#1(C) 123234345456
 x=c("red","yellow","blue","green","magenta","cyan")
 i=seq(1:3)+rep(0:3,each=3)
 x[i]
 
 
-#2.(a)
+#2(a)
 library(spatstat)
 
 mindist <- function(x, y){
@@ -21,7 +21,7 @@ x <- runif(20)
 y <- runif(20)
 mindist(x, y)
 
-#(b)
+#2(b)
 grid <- function(x){
   if(x[1] > 0.5 && x[2] > 0.5){index <- 1
   }else if(x[1] < 0.5 && x[2] > 0.5){index <- 2
@@ -37,7 +37,7 @@ abline(h = 0.5, v = 0.5, lty=2)
 symbols(x, y, circles=index, add=T)
 
 
-#3.
+#3
 gcd = function(a,b)
 { if (b==0) a else gcd(b,a%%b) }
 
@@ -46,18 +46,69 @@ lcm = function(c,d)
 
 
 
-#4.
+#4(a)
+midsqur <- function(seed,times){
+  numvector<- NULL
+  for(i in 1:times){
+    num <- seed*seed
+    seed <- (num %/% 1000) %% 1000000
+    #num (1)先除以1000取整數部分，等同於去掉後三位數
+    #(2)將剩下的數值除以1000000取餘數部分，
+    #若剩下的數大於六位數，則取六位數
+    #若少於六位數，則直接取該數
+    numvector<-c(numvector,seed)
+  }
+  return(numvector)
+}
 
-#(b)
+(x <- ceiling(runif(1,0,999999))) #seed
+(x1 <- midsqur(x,10000))
+x1 <- (x1/10^6)
+hist(x1)
+ks.test(x1, y="punif")
+
+for(i in 1:length(x1)){
+  y <- x1[i]-x1
+  ys <- sum(y == 0)
+  yc <- x1[which(y == 0)]
+}
+ys
+yc
+
+
+#4(b)
 x1 <- 0.6
 x2 <- 0
 for (i in 1:10000){
   x1 <- (69069*x1) %% 2^32
   x2 <- c(x2, x1)
 }
-x2 <- x2[-1]/2^32
+(x2 <- x2[-1]/2^32)
 hist(x2)
 
-ks.test(x2, 1)
+v1<-floor(x2*10) #分組以0.1為級距
+table(v1)
+ks.test(x2, y = "punif")
+chisq.test(table(v1))
+
+
+#4(c)
+xi<-rnorm(1,0,1)
+yi<-rnorm(1,0,1)
+zi<-rnorm(1,0,1)
+vector <- NULL
+
+for (j in 1:10000) {
+  xi <- (171*xi) %% 30269
+  yi <- (172*yi) %% 30307
+  zi <- (170*zi) %% 30323
+  ui <- ((xi/30269)+(yi/30307)+(zi/30323)) %% 1
+  vector <- c(vector,ui)
+}
+
+v1<-floor(vector*10) #分組以0.1為級距
+table(v1)
+ks.test(vector,y="punif")
+chisq.test(table(v1))
 
 
