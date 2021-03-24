df263bbe41d32479826c7d48be2eb63dd9e2361f
scwu110927 <scwu110927@gmail.com>
2021-03-24 20:44:28 +0800

    Update HW1.R

--- HW1.R
+++ HW1.R
@@ -1,16 +1,17 @@
+##-HW1-################
 #1(a)
-rep(seq(0,4),each=5)
+rep(seq(0,4), each = 5)
 
 #1(b)
-seq(1:5) + rep(0:4,each=5)
+seq(1:5) + rep(0:4, each = 5)
 
-#1(C) 123234345456
-x=c("red","yellow","blue","green","magenta","cyan")
-i=seq(1:3)+rep(0:3,each=3)
+#1(C) 
+x <- c("red","yellow","blue","green","magenta","cyan")
+i <- seq(1:3) + rep(0:3, each = 3)
 x[i]
 
 
-#2(a)
+#2(a)-################
 library(spatstat)
 
 mindist <- function(x, y){
@@ -28,16 +29,17 @@ grid <- function(x){
   }else if(x[1] < 0.5 && x[2] < 0.5){index <- 3
   }else {index <- 4}
 }
+xy <- rbind(x,y)
 index <- apply(xy, 2, grid)
 
 plot(x, y, pch=index)
 abline(h = 0.5, v = 0.5, lty=2)
 
-#(c)rectangles
-symbols(x, y, circles=index, add=T)
+#2(c)
+symbols(x, y, circles = index, add = T)
 
 
-#3
+#3-################
 gcd = function(a,b)
 { if (b==0) a else gcd(b,a%%b) }
 
@@ -45,35 +47,28 @@ lcm = function(c,d)
 { return(c*d/gcd(c,d)) }
 
 
-
-#4(a)
+#4(a)-################
 midsqur <- function(seed,times){
-  numvector<- NULL
+  numvector <- NULL
   for(i in 1:times){
-    num <- seed*seed
-    seed <- (num %/% 1000) %% 1000000
-    #num (1)先除以1000取整數部分，等同於去掉後三位數
-    #(2)將剩下的數值除以1000000取餘數部分，
-    #若剩下的數大於六位數，則取六位數
-    #若少於六位數，則直接取該數
-    numvector<-c(numvector,seed)
+    num <- seed * seed
+    seed <- (num%/%1000) %% 1000000
+    numvector <- c(numvector,seed)
   }
   return(numvector)
 }
 
-(x <- ceiling(runif(1,0,999999))) #seed
-(x1 <- midsqur(x,10000))
-x1 <- (x1/10^6)
+x <- ceiling(runif(1, 0, 999999)) #seed
+x1 <- midsqur(x, 10000)
+x1 <- (x1 / 10^6)
 hist(x1)
-ks.test(x1, y="punif")
+ks.test(x1, y = "punif")
 
 for(i in 1:length(x1)){
-  y <- x1[i]-x1
-  ys <- sum(y == 0)
+  y <- x1[i] - x1
   yc <- x1[which(y == 0)]
 }
-ys
-yc
+table(yc)
 
 
 #4(b)
@@ -83,92 +78,85 @@ for (i in 1:10000){
   x1 <- (69069*x1) %% 2^32
   x2 <- c(x2, x1)
 }
-(x2 <- x2[-1]/2^32)
+x2 <- x2[-1] / 2^32
 hist(x2)
 
-v1<-floor(x2*10) #分組以0.1為級距
-table(v1)
+v <- floor(x2 * 10) 
+chisq.test(table(v))
 ks.test(x2, y = "punif")
-chisq.test(table(v1))
 
 
 #4(c)
-xi<-rnorm(1,0,1)
-yi<-rnorm(1,0,1)
-zi<-rnorm(1,0,1)
+xi <- rnorm(1, 0, 1)
+yi <- rnorm(1, 0, 1)
+zi <- rnorm(1, 0, 1)
 vector <- NULL
 
 for (j in 1:10000) {
   xi <- (171*xi) %% 30269
   yi <- (172*yi) %% 30307
   zi <- (170*zi) %% 30323
-  ui <- ((xi/30269)+(yi/30307)+(zi/30323)) %% 1
-  vector <- c(vector,ui)
+  ui <- ((xi/30269) + (yi/30307) + (zi/30323)) %% 1
+  vector <- c(vector, ui)
 }
 
-v1<-floor(vector*10) #分組以0.1為級距
-table(v1)
-ks.test(vector,y="punif")
-chisq.test(table(v1))
+v <- floor(x2 * 10) 
+chisq.test(table(v))
+ks.test(vector, y = "punif")
 
-#5(a)
-t1=NULL
-be=10000/15
+
+#5(a)-################
+t1 <- NULL
+be <- 10000/15
 for (i in 1:1000) {
-  a1=sample(c(1:15), 10000, T)
-  a2=ceiling(15*runif(10000))
-  b1=table(a1)
-  b2=table(a2)
-  c1=sum((b1-be)^2/be)
-  c2=sum((b2-be)^2/be)
-  d1=pchisq(c1,14)
-  d2=pchisq(c2,14)
-  t1=cbind(t1,c(d1,d2))
+  a1 <- sample(c(1:15), 10000, T)
+  a2 <- ceiling(15 * runif(10000))
+  b1 <- table(a1)
+  b2 <- table(a2)
+  c1 <- sum((b1-be)^2 / be)
+  c2 <- sum((b2-be)^2 / be)
+  d1 <- pchisq(c1, 14)
+  d2 <- pchisq(c2, 14)
+  t1 <- cbind(t1,c(d1,d2))
 }
+par(mfrow = c(1, 2))
 hist(t1[1,])
-v1<-table(floor(t1[1,]*10))
-chisq.test(v1)
 hist(t1[2,])
-v2<-table(floor(t1[2,]*10))
+
+v1 <- table(floor(t1[1,] * 10))
+chisq.test(v1)
+v2 <- table(floor(t1[2,] * 10))
 chisq.test(v2)
 
 #5(b)
-#phi
-casio <- function(seed,times){
-  phi <- (1+sqrt(5))/2
+casio <- function(seed, times){
+  phi <- (1+sqrt(5)) / 2
   uvector <- NULL
   for(i in 1:times){
     seed <- ((phi+seed)^5) %% 1
-    uvector<-c(uvector,seed)
+    uvector <- c(uvector,seed)
   }
   return(uvector)
 }
 
-(l <- runif(1,0,1))
-k <- casio(l,10000)
-hist(k)
-
-v1<-floor(k*10) #分組以0.1為級距
-#hist(v1)
-table(v1)
-
-
-#sqrt(2)
 casio2 <- function(seed,times){
   uvector <- NULL
   for(i in 1:times){
     seed <- ((sqrt(2)+seed)^5) %% 1
-    uvector<-c(uvector,seed)
+    uvector <- c(uvector,seed)
   }
   return(uvector)
 }
 
-k2<-casio2(l,10000)
+l <- runif(1, 0, 1)
+k <- casio(l, 10000)
+k2 <- casio2(l, 10000)
+par(mfrow = c(1, 2))
+hist(k)
 hist(k2)
 
-v2<-floor(k2*10) #分組以0.1為級距
-#hist(v1)
-table(v2)
-
+v1 <- floor(k * 10)
 chisq.test(table(v1))
+v2 <- floor(k2 * 10)
 chisq.test(table(v2))
+
