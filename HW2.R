42c051b489b3bccb6132d29da70e9cfdae5e4f18
scwu110927 <scwu110927@gmail.com>
2021-04-01 12:03:43 +0800

    Update HW2.R

--- HW2.R
+++ HW2.R
@@ -1,3 +1,13 @@
 ##-HW1-################
 #1(a)
 
+
+
+#2(a)
+pi <- read.table("StatisticalSimulation/pi.txt", colClasses="character")
+pi.digit <- as.numeric(strsplit(as.character(pi), "")[[1]][-c(1:2)])
+
+hist(pi.digit)
+chisq.test(table(pi.digit))
+
+
